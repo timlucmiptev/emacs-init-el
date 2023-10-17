361e419aa1aa9386cb2180c5ad72fdbc46a856be
timlucmiptev <info@timlucmiptev.space>
2023-10-17 10:31:16 +0200

    magit and markdown

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(cider monokai-theme helm undo-tree paredit clojure-mode avy)))
+   '(magit markdown-mode cider monokai-theme helm undo-tree paredit clojure-mode avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
